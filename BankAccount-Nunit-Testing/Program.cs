8ca59fb64d7c55f6b9d9f305641a418978e8a5fa
hampus <hampus.john.olausson@gmail.com>
2021-09-01 11:06:29 +0200

    Added a bank account file with multiple options

---
+++ BankAccount-Nunit-Testing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankAccount_Nunit_Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
