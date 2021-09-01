b9e919171ad923ab3a7970fcf839aef00f4d5d34
hampus <hampus.john.olausson@gmail.com>
2021-09-01 11:29:41 +0200

    * UnitTest1.cs:
    * BankAccountTests.cs: First Nunit test adding 500 to bankaccount and
      check

---
+++ Bank.NunitTests/BankAccountTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Bank;
+
+namespace Bank.NunitTests
+{
+    public class BackAccountTests
+    {
+        
+        [Test]
+        public void Adding_Funds_Updates_Balance()
+        {
+            // Arrange
+            var account = new BankAccount(1000);
+
+            // Act
+            account.Add(500);
+
+            // Assert
+            Assert.AreEqual(1500, account.Balance);
+
+        }
+    }
+}
